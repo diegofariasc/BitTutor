dbb37988225d274b75a475e5b6135411a1eef440
Diego Farias Castro <diegofariascastro@MacBook-Air-de-Diego.local>
2020-10-19 08:04:10 -0500

    Triggers for wishing, subscribing and banning status switching

--- BuildDatabase.sql
+++ BuildDatabase.sql
@@ -1,4 +1,3 @@
-
 -- Drop previous database constructions and re-build
 DROP DATABASE IF EXISTS BitTutor;
 CREATE DATABASE BitTutor;
@@ -76,7 +75,9 @@ CREATE TABLE FILE_RESOURCE (    name            VARCHAR(766)    NOT NULL,
                                                         format = 'txt' OR
                                                         format = 'file' ),
 
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (name, course) );
 
 -- Intermediate table between file_resource and user table
@@ -85,32 +86,60 @@ CREATE TABLE HAS_ACCESSED ( resourceName    VARCHAR(766)    NOT NULL,
                             user            INTEGER         NOT NULL,
 
                             FOREIGN KEY (resourceName) REFERENCES FILE_RESOURCE (name),
-                            FOREIGN KEY (course) REFERENCES COURSE (id),
-                            FOREIGN KEY (user) REFERENCES USER (id),
+                            FOREIGN KEY (course) REFERENCES COURSE (id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,                            
+                            FOREIGN KEY (user) REFERENCES USER (id) 
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
                             PRIMARY KEY (resourceName, course, user) );
 
 -- Intermediate table between course and user table with suscribing relation
-CREATE TABLE SUSCRIBES (user    INTEGER NOT NULL,
+CREATE TABLE SUBSCRIBES (user    INTEGER NOT NULL,
                         course  INTEGER NOT NULL,
 
-                        FOREIGN KEY (user) REFERENCES USER (id),
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (user) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (user,course) );
 
 -- Intermediate table between course and user table with teaching relation
 CREATE TABLE TEACHES (  user    INTEGER NOT NULL,
                         course  INTEGER NOT NULL,
 
-                        FOREIGN KEY (user) REFERENCES USER (id),
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (user) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (user,course) );
 
 -- Intermediate table between course and user table with user banning relation
 CREATE TABLE IS_BANNED (user    INTEGER NOT NULL,
                         course  INTEGER NOT NULL,
 
-                        FOREIGN KEY (user) REFERENCES USER (id),
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (user) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        PRIMARY KEY (user,course) );
+
+-- Intermediate table between course and user table with whishing relation
+CREATE TABLE WISHES    (user    INTEGER NOT NULL,
+                        course  INTEGER NOT NULL,
+
+                        FOREIGN KEY (user) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (user,course) );
 
 -- Intermediate table between course and user table with completing relation
@@ -118,8 +147,12 @@ CREATE TABLE COMPLETES (user    INTEGER NOT NULL,
                         course  INTEGER NOT NULL,
                         date    DATE NOT NULL,
 
-                        FOREIGN KEY (user) REFERENCES USER (id),
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (user) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (user,course) );
 
 -- Intermediate table between course and user table with reviewing relation
@@ -130,8 +163,12 @@ CREATE TABLE REVIEWS (  author      INTEGER NOT NULL,
 
                         CONSTRAINT validStars CHECK ( stars >= 1  AND stars <= 5 ),
 
-                        FOREIGN KEY (author) REFERENCES USER (id),
-                        FOREIGN KEY (course) REFERENCES COURSE (id),
+                        FOREIGN KEY (author) REFERENCES USER (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                        FOREIGN KEY (course) REFERENCES COURSE (id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
                         PRIMARY KEY (author,course) );
 
 -- Quiz table
@@ -140,7 +177,9 @@ CREATE TABLE QUIZ ( id              INTEGER NOT NULL,
                     title           VARCHAR(1024) NOT NULL,
                     instructions    MEDIUMTEXT,
 
-                    FOREIGN KEY (course) REFERENCES COURSE (id),
+                    FOREIGN KEY (course) REFERENCES COURSE (id)
+                    ON DELETE CASCADE
+                    ON UPDATE CASCADE,
                     PRIMARY KEY (id) );
 
 -- Question table
@@ -167,5 +206,52 @@ CREATE TABLE GETS_RESULT (  quizId          INTEGER NOT NULL,
                             correctAnswers  INTEGER NOT NULL,
 
                             FOREIGN KEY (quizId) REFERENCES QUIZ (id),
-                            FOREIGN KEY (user) REFERENCES USER (id),
+                            FOREIGN KEY (user) REFERENCES USER (id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE,
                             PRIMARY KEY (quizId,user) );
+
+DELIMITER $$
+
+    CREATE TRIGGER switchFromWishToSubscribed
+    AFTER INSERT
+    ON SUBSCRIBES FOR EACH ROW
+
+    BEGIN
+        
+        DELETE FROM WISHES WHERE 
+        WISHES.user = NEW.user AND WISHES.course = NEW.course;
+
+    END$$    
+
+DELIMITER ;
+
+DELIMITER $$
+
+    CREATE TRIGGER switchFromSubscribedToCompleted
+    AFTER INSERT
+    ON COMPLETES FOR EACH ROW
+
+    BEGIN
+        
+        DELETE FROM SUBSCRIBES WHERE 
+        SUBSCRIBES.user = NEW.user AND SUBSCRIBES.course = NEW.course;
+
+    END$$    
+
+DELIMITER ;
+
+DELIMITER $$
+
+    CREATE TRIGGER removeSubscriptionAfterBanning
+    AFTER INSERT
+    ON IS_BANNED FOR EACH ROW
+
+    BEGIN
+        
+        DELETE FROM SUBSCRIBES WHERE 
+        SUBSCRIBES.user = NEW.user AND SUBSCRIBES.course = NEW.course;
+
+    END$$    
+
+DELIMITER ;
